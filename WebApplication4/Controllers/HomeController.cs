b14d2d37878906298840eecb377f0b5114f9c6ec
Preety Kaur <preetynz88@gmail.com>
2018-10-14 23:47:11 +1300

    Add project files.

---
+++ WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+        HarpreetEntities1 harpreetEntities1 = new HarpreetEntities1();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult All()
+        {
+            List<Employee> models = harpreetEntities1.Employees.ToList();
+            return PartialView("_Employee", models);
+        }
+
+        public PartialViewResult Top3()
+        {
+            List<Employee> models = harpreetEntities1.Employees.OrderByDescending(x => x.Salary).Take(3).ToList();
+            return PartialView("_Employee", models);
+        }
+
+        public PartialViewResult Bottom3()
+        {
+            List<Employee> models = harpreetEntities1.Employees.OrderBy(x => x.Salary).Take(3).ToList();
+            return PartialView("_Employee", models);
+        }
+    }
+}
